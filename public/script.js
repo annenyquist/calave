15f8fd14b136a64463b0c5b39981dc0bbdac5d39
Anne Nyquist <anne@annenyquist.com>
2016-08-21 13:09:13 -0700

    Added popups to concept images

--- public/script.js
+++ public/script.js
@@ -64,3 +64,13 @@ calaveApp.controller('researchController',
       $anchorScroll();
    }
 });
+
+function show(icon) {
+  var popupId = icon.parentNode.id.replace(/icon/i, 'popup');
+  document.getElementById(popupId).style.display = 'block';
+}
+
+function hide(icon) {
+  var popupId = icon.parentNode.id.replace(/icon/i, 'popup');
+  document.getElementById(popupId).style.display = 'none';
+}
